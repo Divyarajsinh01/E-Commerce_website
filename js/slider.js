612a48d7a67357b8e080c296331f17acd61e43b0
Divyarajsinh01 <141410024+Divyarajsinh01@users.noreply.github.com>
2023-08-14 19:48:11 +0530

    e-comm

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 360,
+        stretch: 0,
+        depth: 300,
+        modifier: 5,
+        slideShadows: false,
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
